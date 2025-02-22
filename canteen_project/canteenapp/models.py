4e562c4decec692c80b2dcc64db8bc2e96a50610
DyvinBiju <dyvinbiju@gmail.com>
2025-02-22 12:29:49 +0530

    first model

--- canteen_project/canteenapp/models.py
+++ canteen_project/canteenapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class FoodItems(models.Model):
+    food_items = models.CharField(max_length=30)
+    image = models.ImageField(upload_to='canteen_images/',null=True,blank=True)
+    description = models.TextField(blank=True)
+    f_stock = models.IntegerField()
+    price = models.DecimalField(max_digits=10,decimal_places=2)
+    student = models.ForeignKey(User,on_delete=models.CASCADE)
