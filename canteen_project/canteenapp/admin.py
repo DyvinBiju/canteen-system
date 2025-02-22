93db4eab1272d43850b2e00eabe91caf1fc899d6
DyvinBiju <dyvinbiju@gmail.com>
2025-02-22 16:15:16 +0530

    model completed

--- canteen_project/canteenapp/admin.py
+++ canteen_project/canteenapp/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from.models import FoodItems,orders,Feedback
+from.models import FoodItems,orders,Feedback,OrderItems
 admin.site.register(FoodItems)
 admin.site.register(orders)
 admin.site.register(Feedback)
+admin.site.register(OrderItems)
--- canteen_project/canteenapp/models.py
+++ canteen_project/canteenapp/models.py
@@ -1,23 +1,42 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class FoodItems(models.Model):
-    food_items = models.CharField(max_length=30)
+class TimeStampedModel(models.Model):
+     created_at = models.DateTimeField(auto_now=True)
+     updated_at = models.DateTimeField(auto_now=True)
+
+     class Meta:
+          abstract = True
+
+class FoodItems(TimeStampedModel):
+    
+    name = models.CharField(max_length=30)
     image = models.ImageField(upload_to='canteen_images/',null=True,blank=True)
     description = models.TextField(blank=True)
     f_stock = models.IntegerField()
     price = models.DecimalField(max_digits=10,decimal_places=2)
     student = models.ForeignKey(User,on_delete=models.CASCADE)
 
-class orders(models.Model):
-    no_of_items_ordered = models.IntegerField()
+    def __str__(self):
+        return self.name
+    
+
+class orders(TimeStampedModel):
+    
     total_price = models.IntegerField()
     student = models.ForeignKey(User,on_delete=models.CASCADE)
     food = models.ForeignKey(FoodItems,on_delete=models.CASCADE)
+    order_date = models.DateTimeField(auto_now_add=True)
 
-class Feedback(models.Model):
+class Feedback(TimeStampedModel):
     student = models.ForeignKey(User, on_delete=models.CASCADE)
     food_item = models.ForeignKey(FoodItems, on_delete=models.CASCADE, null=True, blank=True)
     rating = models.IntegerField(choices=[(i, i) for i in range(1, 6)])  # Rating from 1 to 5
     comments = models.TextField(blank=True)
     submission_date = models.DateTimeField(auto_now_add=True)
+
+class OrderItems(TimeStampedModel):
+     food = models.ForeignKey(FoodItems,on_delete=models.CASCADE)
+     orders = models.ForeignKey(orders,on_delete=models.CASCADE)
+     quantity = models.IntegerField()
+     price = models.DecimalField(max_digits=10,decimal_places=2)
