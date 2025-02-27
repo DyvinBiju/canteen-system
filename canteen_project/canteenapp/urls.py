78a425bcc5a268049f5eafe002dade1d33cca7c9
Amal krishna .B <amalefc123@gmail.com>
2025-02-27 17:11:34 +0530

    food_list.html

--- canteen_project/canteenapp/urls.py
+++ canteen_project/canteenapp/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
 
 path('fdgf', views.index, name='index'),
-path('jjjj', views.home, name='home'),
+path('', views.home, name='home'),
 path('layout/', views.layout, name='layout'),
 path('foodlist/', views.foodlist, name='foodlist'),
 
